ac4b2cd64f969996b242e49af7bfd80c63dcbf82
Timi Ruprecht <Ruprecht.t@gmx.de>
2019-08-05 16:05:08 +0200

    Tag: other
    remove unnecessary spring component annotation on exception

--- src/main/java/ch/deeppay/util/GeneralException.java
+++ src/main/java/ch/deeppay/util/GeneralException.java
@@ -3,13 +3,10 @@ package ch.deeppay.util;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 @EqualsAndHashCode(callSuper = true)
-@Component
 @Data
 public class GeneralException extends RuntimeException {
 
@@ -26,14 +23,5 @@ public class GeneralException extends RuntimeException {
   public GeneralException(final @NonNull String message, final @NonNull HttpStatus httpStatus) {
     super(message);
     this.httpStatus = httpStatus;
-  }
-
-  @Autowired
-  public GeneralException(MessageSource messageSource) {
-    this.messageSource = messageSource;
-  }
-
-  MessageSource getMessageSource() {
-    return this.messageSource;
   }
 }
