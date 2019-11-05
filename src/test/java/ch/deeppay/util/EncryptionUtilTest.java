d8f01d7b7094b585d5e0d6dc7a89d55618329b2f
Timotheus Ruprecht <timotheus.ruprecht@abacus.ch>
2019-11-05 07:26:35 +0100

    Bugfix - missing url encoding

--- src/test/java/ch/deeppay/util/EncryptionUtilTest.java
+++ src/test/java/ch/deeppay/util/EncryptionUtilTest.java
@@ -3,7 +3,9 @@ package ch.deeppay.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TransportDataUtilTest {
+import java.util.Optional;
+
+class EncryptionUtilTest {
 
   @Test
   void encrypt() throws Exception {
@@ -12,12 +14,13 @@ class TransportDataUtilTest {
 
     String transportData = "b=testtest!a=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!r=rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr!i=iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!is=ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
 
-    String encrypt = TransportDataUtil.encrypt(transportData, secret, salt);
-    String encryptTwice = TransportDataUtil.encrypt(transportData, secret, salt);
+    Optional<String> encrypt = EncryptionUtil.encrypt(transportData, secret, salt);
+    Optional<String> encryptTwice = EncryptionUtil.encrypt(transportData, secret, salt);
 
-    Assertions.assertNotNull(encrypt);
-    Assertions.assertNotNull(encryptTwice);
-    Assertions.assertEquals(encrypt, encryptTwice);
+    Assertions.assertTrue(encrypt.isPresent());
+    Assertions.assertTrue(encryptTwice.isPresent());
+
+    Assertions.assertEquals(encrypt.get(), encryptTwice.get());
   }
 
   @Test
@@ -25,11 +28,12 @@ class TransportDataUtilTest {
     String secret = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
     String salt = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
 
-    String encrypt = "eGgedBv87cL2t0h7dNuxfDyYWFzT1i2FQPWEpEQjTu7ztXxrq0iSau7y2J8uaB0Nzm37HOYdoDRZLnZIPmB3ivDn/3vNbXEk6Zw9YIeiiHZnbf+HVO3VPf+DH4yIZisEpQVu/PBYpzqHQ+hdDtUXoa0YXVxvsdKnhjpj+rrTeAV8Vu9iCuAbncmY/3KDq6walVR6tWzKKk91zxFB9RDODiUzmlfCi6TcXppgZBUzXT+ZcXLTU2pZNDL4aWOkyZFk5MTsouOyl6X0C44OMtRo56537deQrGyWqv/L1TTNgYQ6FZqKYvznGoUJgfXXCN6M9hgEjh+D99v8EV0wb9Oijw==";
+    String encrypt = "eGgedBv87cL2t0h7dNuxfDyYWFzT1i2FQPWEpEQjTu7ztXxrq0iSau7y2J8uaB0Nzm37HOYdoDRZLnZIPmB3ivDn%2F3vNbXEk6Zw9YIeiiHZnbf%2BHVO3VPf%2BDH4yIZisEpQVu%2FPBYpzqHQ%2BhdDtUXoa0YXVxvsdKnhjpj%2BrrTeAV8Vu9iCuAbncmY%2F3KDq6walVR6tWzKKk91zxFB9RDODiUzmlfCi6TcXppgZBUzXT%2BZcXLTU2pZNDL4aWOkyZFk5MTsouOyl6X0C44OMtRo56537deQrGyWqv%2FL1TTNgYQ6FZqKYvznGoUJgfXXCN6M9hgEjh%2BD99v8EV0wb9Oijw%3D%3D";
     String transportData = "b=testtest!a=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!r=rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr!i=iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!is=ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";
 
-    String decrypt = TransportDataUtil.decrypt(encrypt, secret, salt);
-    Assertions.assertEquals(transportData, decrypt);
+    Optional<String> decrypt = EncryptionUtil.decrypt(encrypt, secret, salt);
+    Assertions.assertTrue(decrypt.isPresent());
+    Assertions.assertEquals(transportData, decrypt.get());
   }
 
 }
