118150e1f2af5efa5c6c7fd919765d818baebf34
Stefan Geissbuehler <stefan.geissbuehler@abacus.ch>
2020-12-02 07:08:37 +0100

    adjusting file names

--- src/main/java/ch/deeppay/util/FileFormat.java
+++ src/main/java/ch/deeppay/util/FileFormat.java
@@ -1,44 +1,51 @@
 package ch.deeppay.util;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-import org.springframework.web.server.ResponseStatusException;
-
+import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.web.server.ResponseStatusException;
+
 public enum FileFormat {
 
-  MT940("MT940", true, false),
-  MT942("MT942", true, false),
-  CAMT052("CAMT052", true, false),
-  CAMT053("CAMT053", true, false),
-  CAMT054("CAMT054", true, false),
-  CAMT054_CDTN_DBTN("CAMT054_CDTN_DBTN", true, false),
-  CAMT054_ESR_LSV("CAMT054_ESR_LSV", true, false),
-  CAMT054_ZA("CAMT054_ZA", true, false),
+  MT940("MT940", ".sta", true, false),
+  MT942("MT942", ".sta", true, false),
+  CAMT052("CAMT052", ".c52.xml", true, false),
+  CAMT053("CAMT053", ".c53.xml", true, false),
+  CAMT054("CAMT054", ".c54.xml", true, false),
+  CAMT054_CDTN_DBTN("CAMT054_CDTN_DBTN", ".cs2.xml", true, false),
+  CAMT054_ESR_LSV("CAMT054_ESR_LSV", ".xml", true, false),
+  CAMT054_ZA("CAMT054_ZA", ".xml", true, false),
 
-  ESR("ESR", true, false),
-  LSV("LSV", true, true),
-  EDOC("EDOC", true, false),
-  MT571("MT571", true, false),
+  ESR("ESR", ".v11", true, false),
+  LSV("LSV", ".lsv", true, true),
+  EDOC("EDOC", ".pdf", true, false),
 
-  DTA("DTA", false, true),
-  PAIN001("PAIN001", false, true),
-  PAIN002("PAIN002", true, false),
-  PAIN008("PAIN008", false, true);
+  DTA("DTA", ".dta", false, true),
+  PAIN001("PAIN001", ".xml", false, true),
+  PAIN002("PAIN002", ".z01.xml", true, false),
+  PAIN008("PAIN008", ".xml", false, true);
 
   private final String name;
+  private final String fileExtension;
   private final boolean upload;
   private final boolean download;
 
-  FileFormat(@NonNull final String name, final boolean download, final boolean upload) {
+  FileFormat(@NonNull final String name, @Nonnull final String fileExtension, final boolean download, final boolean upload) {
     this.name = name;
+    this.fileExtension = fileExtension;
     this.upload = upload;
     this.download = download;
+  }
+
+  @Nonnull
+  public String getFileExtension() {
+    return fileExtension;
   }
 
   public boolean isDownload() {
