2fd82ae9dfc537fdd8624f18e2e1445f209a1924
Stefan Geissbuehler <stefan.geissbuehler@abacus.ch>
2020-12-07 15:18:58 +0100

    Issue EPAY-11088: new challenge type

--- src/main/java/ch/deeppay/util/LoginState.java
+++ src/main/java/ch/deeppay/util/LoginState.java
@@ -3,11 +3,12 @@ package ch.deeppay.util;
 /**
  * Possible bank login procedures.
  *
- * https://wiki.abacus.ch/display/EPAY/API+Design
+ * https://wiki.deeppay.swiss/bin/view/Entwicklung/Architektur/API%20Design/
  */
 public enum LoginState {
   CHANGE_PASSWORD,
   MOBILE_TAN,
+  PUSH_TAN,
   ONE_TIME_PASSWORD,
   OPTICAL_CHALLENGE,
   GRID_CARD,
