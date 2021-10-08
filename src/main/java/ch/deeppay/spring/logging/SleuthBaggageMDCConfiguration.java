fe029cf1baeb667d8a2cec804f1abb4e8d4aa132
Stefan Geissbuehler <stefan.geissbuehler@abacus.ch>
2021-10-08 16:05:12 +0200

    fix session trace id bean qualifier

--- src/main/java/ch/deeppay/spring/logging/SleuthBaggageMDCConfiguration.java
+++ src/main/java/ch/deeppay/spring/logging/SleuthBaggageMDCConfiguration.java
@@ -19,27 +19,21 @@ public class SleuthBaggageMDCConfiguration {
   public static final String X_CLIENT_TYPE = "X-ClientType";
   public static final String CLIENT_TYPE_MDC_NAME = "clientType";
 
-  @Bean(name = "BaggageFieldClientType")
-  @ConditionalOnMissingBean
-  BaggageField clientType() {
-    return BaggageField.create(X_CLIENT_TYPE);
-  }
-
   @Bean(name = "BaggageFieldSessionTraceId")
   @ConditionalOnMissingBean
-  BaggageField sessionTraceId() {
+  public BaggageField getBaggageFieldSessionTraceId() {
     return BaggageField.create(X_B3_SESSION_TRACE_ID);
   }
 
   @Bean
   @ConditionalOnMissingBean
-  CurrentTraceContext.ScopeDecorator mdcScopeDecorator() {
+  public CurrentTraceContext.ScopeDecorator mdcScopeDecorator() {
     return MDCScopeDecorator.newBuilder()
                             .clear()
-                            .add(newBuilder(clientType()).name(CLIENT_TYPE_MDC_NAME)
+                            .add(newBuilder(BaggageField.create(X_CLIENT_TYPE)).name(CLIENT_TYPE_MDC_NAME)
                                                          .flushOnUpdate()
                                                          .build())
-                            .add(newBuilder(sessionTraceId()).name(SESSION_TRACE_ID_MDC_NAME)
+                            .add(newBuilder(getBaggageFieldSessionTraceId()).name(SESSION_TRACE_ID_MDC_NAME)
                                                              .flushOnUpdate()
                                                              .build())
                             .build();
