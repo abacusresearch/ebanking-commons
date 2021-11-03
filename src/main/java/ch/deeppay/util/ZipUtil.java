9bf8b3e1f2b001210278563ebc5b959b4a072b1f
Stefan Geissbuehler <stefan.geissbuehler@abacus.ch>
2021-11-03 11:24:37 +0100

    fix zip

--- src/main/java/ch/deeppay/util/ZipUtil.java
+++ src/main/java/ch/deeppay/util/ZipUtil.java
@@ -12,8 +12,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -27,19 +27,26 @@ import java.util.zip.ZipOutputStream;
 
 import ch.deeppay.exception.DeepPayProblemException;
 import ch.deeppay.models.ebanking.server.StringFile;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.CompressionMethod;
-import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import static org.apache.commons.codec.binary.Base64.decodeBase64;
+
 @SuppressWarnings("unused")
 public class ZipUtil {
+
+  public static final String DATE_TIME_FORMAT = "yyyy-MM-dd-HH-mm-ss";
 
   private static final Logger LOGGER = Logger.getLogger(ZipUtil.class.getName());
 
@@ -52,13 +59,12 @@ public class ZipUtil {
       // create dir for all files
       final File tmpDirectory = Files.createTempDirectory("folder").toFile();
       try {
-        int filenumber = 0;
+        int counter = 0;
         for (StringFile file : files) {
-          final File current = generateFile(file, tmpDirectory, ++filenumber);
+          final File current = generateFile(file, tmpDirectory, ++counter);
           writeToFile(current, file.getFileContent());
         }
         final File zipFile = createZipFile(tmpDirectory);
-
         final String result = encodeFileToString(zipFile.getPath());
         if (Objects.isNull(result)) {
           return StringUtils.EMPTY;
@@ -88,11 +94,19 @@ public class ZipUtil {
     }
   }
 
-  public static boolean isZipFile(@NonNull final String fileContent) {
-    return isZipFile(fileContent.getBytes(Charsets.UTF_8));
+  public static boolean isZipFile(@Nullable final String fileContent) {
+    if(StringUtils.isEmpty(fileContent)){
+      return false;
+    }
+
+    return isZipFile(fileContent.getBytes(StandardCharsets.UTF_8));
   }
 
-  public static boolean isZipFile(@NonNull final byte[] fileContent) {
+  public static boolean isZipFile(@Nullable final byte[] fileContent) {
+    if(ArrayUtils.isEmpty(fileContent)){
+      return false;
+    }
+
     try (ByteArrayInputStream is = new ByteArrayInputStream(fileContent)) {
       return new ZipInputStream(is).getNextEntry() != null;
     } catch (IOException e) {
@@ -100,14 +114,18 @@ public class ZipUtil {
     }
   }
 
-  private static File createZipFile(@NonNull final File tmpDirectory) throws ZipException {
+  public static File createZipFile(@NonNull final File file) throws ZipException {
     // DO not use -> File.createTempFile -> does not work with ZipFile library
     File zipFile = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID() + ".zip");
     ZipFile myZipFile = new ZipFile(zipFile);
 
     ZipParameters parameters = new ZipParameters();
     parameters.setCompressionMethod(CompressionMethod.DEFLATE);
-    myZipFile.addFolder(tmpDirectory, parameters);
+    if (file.isDirectory()) {
+      myZipFile.addFolder(file, parameters);
+    } else {
+      myZipFile.addFile(file);
+    }
     return zipFile;
   }
 
@@ -136,23 +154,32 @@ public class ZipUtil {
     }
   }
 
-  private static File generateFile(@NonNull StringFile file,
-                            @NonNull final File tmpDirectory,
-                            final int count) {
-    return StringUtils.isNotBlank(file.getFileName())
-        ? new File(tmpDirectory,file.getFileName())
-        : generateFile(file.getFormat(), tmpDirectory, count);
+  public static File generateFile(@NonNull StringFile file, @NonNull final File tmpDirectory, final int count) {
+    return StringUtils.isBlank(file.getFileName()) ? generateFile(file.getFormat(), tmpDirectory, count) : new File(tmpDirectory, file.getFileName());
   }
 
-  public static File generateFile(@NonNull String fileFormat,
-                                  @NonNull final File tmpDirectory,
-                                  final int count) {
-    Date date = new Date();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-    String name = fileFormat + '_' + dateFormat.format(date) + '_' + count;
-    return new File(tmpDirectory,name);
+  public static File generateFile(@NonNull String fileFormat, @NonNull final File tmpDirectory, final int count) {
+    return new File(tmpDirectory, generateFileName(fileFormat, count));
   }
 
+  public static File generateFile(@NonNull final File tmpDirectory, final int count) {
+    return new File(tmpDirectory, generateFileName(null, count));
+  }
+
+  public static String generateFileName(@Nullable String fileFormat, final int count) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
+    StringBuilder builder = new StringBuilder();
+    if (Objects.nonNull(fileFormat)) {
+      builder.append(fileFormat);
+      builder.append("_");
+    }
+
+    builder.append(dateFormat.format(new Date()));
+    builder.append("_");
+    builder.append(count);
+
+    return builder.toString();
+  }
 
   /**
    * Extracts a zip file specified by the content byte array to a directory specified by
@@ -167,33 +194,33 @@ public class ZipUtil {
       throw new RuntimeException(String.format("Failed to create folder %s", destDirectory));
     }
 
-    ZipInputStream zipIn = new ZipInputStream(new ByteArrayInputStream(content));
-    ZipEntry entry = zipIn.getNextEntry();
-    // iterates over entries in the zip file
-    while (entry != null) {
-      String filePath = destDirectory + File.separator + entry.getName();
-      if (entry.isDirectory()) {
-        // if the entry is a directory, make the directory
-        File dir = new File(filePath);
-        if (!dir.exists() && !dir.mkdir()) {
-          throw new RuntimeException(String.format("Failed to create folder %s", destDirectory));
+    try (ZipInputStream zipIn = new ZipInputStream(new ByteArrayInputStream(content))) {
+      ZipEntry entry = zipIn.getNextEntry();
+      // iterates over entries in the zip file
+      while (entry != null) {
+        String filePath = destDirectory + File.separator + entry.getName();
+        if (entry.isDirectory()) {
+          // if the entry is a directory, make the directory
+          File dir = new File(filePath);
+          if (!dir.exists() && !dir.mkdir()) {
+            throw new RuntimeException(String.format("Failed to create folder %s", destDirectory));
+          }
+        } else {
+          // if the entry is a file, extracts it
+          Path path = Paths.get(filePath);
+          FileUtils.createParentDirectories(path.toFile());
+          Files.copy(zipIn, Paths.get(filePath));
         }
-      } else {
-        // if the entry is a file, extracts it
-        Path path = Paths.get(filePath);
-        FileUtils.createParentDirectories(path.toFile());
-        Files.copy(zipIn, Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+        zipIn.closeEntry();
+        entry = zipIn.getNextEntry();
       }
-      zipIn.closeEntry();
-      entry = zipIn.getNextEntry();
     }
-    zipIn.close();
   }
 
   /**
-   * Zip a passed folder and return's it as a byte array
+   * The conent of the passed folder is zipped and return's it as a byte array
    *
-   * @param dir - Folder that should be zipped
+   * @param dir - Content of the folder that should be zipped
    * @return byte array of the zipped folder
    * @throws IOException
    */
@@ -206,7 +233,7 @@ public class ZipUtil {
 
           if (Files.isDirectory(path)) {
             //skip root path element. php does not ignore it
-            if("".equals(relativePath.toString())){
+            if ("".equals(relativePath.toString())) {
               continue;
             }
 
@@ -223,7 +250,29 @@ public class ZipUtil {
     }
   }
 
-  public static byte[] unzip(final byte[] bytes) throws IOException {
+  /**
+   * Unzip does only work for a single zipped file that does not has a subfolder
+   *
+   * @param str zipped string
+   * @return unzipped arrays of bytes
+   * @throws IOException
+   */
+  public static byte[] unzip(final String str) {
+    return isZipFile(str) ? unzip(str.getBytes(StandardCharsets.UTF_8)) : ArrayUtils.EMPTY_BYTE_ARRAY;
+  }
+
+  /**
+   * Unzip does only work for a single zipped file that does not has a subfolder
+   *
+   * @param bytes zipped bytes
+   * @return unzipped arrays of bytes
+   * @throws IOException
+   */
+  public static byte[] unzip(final byte[] bytes) {
+    if (!isZipFile(bytes)) {
+      return ArrayUtils.EMPTY_BYTE_ARRAY;
+    }
+
     final byte[] buffer = new byte[1024];
     try (final ZipInputStream zip = new ZipInputStream(new ByteArrayInputStream(bytes))) {
       try (final ByteArrayOutputStream out = new ByteArrayOutputStream(bytes.length)) {
@@ -235,20 +284,73 @@ public class ZipUtil {
         }
         return out.toByteArray();
       }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 
-  public static byte[] zip(final byte[] bytes) throws IOException {
+  public static byte[] zip(final byte[] content) {
+    return zip(content, "data");
+  }
+
+  /**
+   * Passed byte array is zipped. The file name can contain relative sub folder (e.g. subfolder/myfile.txt)
+   *
+   * @param content  content that should be zipped.
+   * @param fileName name of the zip entry
+   * @return zipped byte array
+   * @throws IOException
+   */
+  public static byte[] zip(final byte[] content, final String fileName) {
+    return zip(Collections.singletonList(EntryData.builder().content(content).fileName(fileName).build()));
+  }
+
+  public static byte[] zip(final List<EntryData> entries) {
+    if (Objects.isNull(entries) || entries.isEmpty()) {
+      return ArrayUtils.EMPTY_BYTE_ARRAY;
+    }
+
     try (final ByteArrayOutputStream output = new ByteArrayOutputStream()) {
       try (final ZipOutputStream zip = new ZipOutputStream(output)) {
         zip.setMethod(ZipOutputStream.DEFLATED);
-        final ZipEntry entry = new ZipEntry("Content");
-        zip.putNextEntry(entry);
-        zip.write(bytes);
-        zip.closeEntry();
+        int count = 0;
+        for (EntryData entryData : entries) {
+          zip.putNextEntry(new ZipEntry(StringUtils.isEmpty(entryData.getFileName()) ? generateFileName(null, ++count) : entryData.getFileName()));
+          zip.write(entryData.getContent());
+          zip.closeEntry();
+        }
       }
       return output.toByteArray();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
+
+
+  /**
+   * The content of the entries can be zipped, base64 encoded or plain text. The content will be decoded and unzipped (if needed)
+   * and packed as a single zipped base64 encoded byte array.
+   *
+   * @param entries list of entries
+   * @return a single base 64 encoded zip file
+   */
+  public static String getBase64EncodedZip(final List<EntryData> entries) {
+    if (Objects.isNull(entries) || entries.isEmpty()) {
+      return StringUtils.EMPTY;
+    }
+    entries.stream().filter(e -> Base64.isBase64(e.getContent())).forEach(e -> e.setContent(decodeBase64(e.getContent())));
+    entries.stream().filter(e -> isZipFile(e.getContent())).forEach(e -> e.setContent(unzip(e.getContent())));
+    return Base64.encodeBase64String(zip(entries));
+  }
+
+  @Builder
+  @AllArgsConstructor
+  @Data
+  public static class EntryData {
+
+    private final String fileName;
+    private byte[] content;
+  }
+
 
 }
--- src/test/java/ch/deeppay/util/ZipUtilTest.java
+++ src/test/java/ch/deeppay/util/ZipUtilTest.java
@@ -7,7 +7,9 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 import ch.deeppay.models.ebanking.server.StringFile;
 import org.apache.commons.io.FileUtils;
@@ -16,6 +18,7 @@ import org.bouncycastle.util.encoders.Base64;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static ch.deeppay.util.ZipUtil.EntryData.builder;
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,65 +29,69 @@ class ZipUtilTest {
   @Test
   void testDeleteDirectory() {
     final File parentFolder = getParentTempDirectory();
-    File[] files = parentFolder.listFiles((dir, name) -> name.startsWith("base64"));
-    for(File f : files) {
+    File[] files = requireNonNull(parentFolder.listFiles((dir, name) -> name.startsWith("base64")));
+    for (File f : files) {
       ZipUtil.deleteDirectory(f);
     }
 
     files = parentFolder.listFiles((dir, name) -> name.startsWith("base64"));
-    Assertions.assertEquals(0,files.length);
+    Assertions.assertEquals(0, files.length);
   }
 
   @Test
   void testGetBase64Zip() throws IOException {
-    StringFile file1 = new StringFile("FILE_CONTENT1", FileFormat.PAIN002.name(),"FILE_NAME1");
-    StringFile file2 = new StringFile("FILE_CONTENT2", FileFormat.PAIN002.name(),"FILE_NAME2");
-    String base64 = ZipUtil.getBase64Zip(Arrays.asList(file1,file2));
+    StringFile file1 = new StringFile("FILE_CONTENT1", FileFormat.PAIN002.name(), "FILE_NAME1");
+    StringFile file2 = new StringFile("FILE_CONTENT2", FileFormat.PAIN002.name(), "FILE_NAME2");
+    String base64 = ZipUtil.getBase64Zip(Arrays.asList(file1, file2));
     Assertions.assertNotNull(base64);
 
     final File tmpDirectory = Files.createTempDirectory("tmp").toFile();
     try {
       ZipUtil.unzip(Base64.decode(base64), tmpDirectory);
-      File[] files = tmpDirectory.listFiles();
-      Assertions.assertEquals(1,files.length);
+      File[] files = requireNonNull(tmpDirectory.listFiles());
+      Assertions.assertEquals(1, files.length);
 
       files = files[0].listFiles();
-      Assertions.assertEquals(2,files.length);
-      Assertions.assertEquals("FILE_NAME1",files[0].getName());
-      Assertions.assertEquals("FILE_NAME2",files[1].getName());
-    }finally {
+      Assertions.assertEquals(2, files.length);
+      Assertions.assertEquals("FILE_NAME1", files[0].getName());
+      Assertions.assertEquals("FILE_NAME2", files[1].getName());
+    } finally {
       FileUtils.deleteDirectory(tmpDirectory);
     }
   }
 
-//  @Test
-//  void testGetBase64ZipWithEmptyFilesNames() throws IOException {
-//    StringFile file1 = new StringFile("FILE_CONTENT1", FileFormat.PAIN002.name(),"");
-//    StringFile file2 = new StringFile("FILE_CONTENT2", FileFormat.PAIN002.name(),"");
-//    String base64 = ZipUtil.getBase64Zip(Arrays.asList(file1,file2));
-//    Assertions.assertNotNull(base64);
-//
-//    final File tmpDirectory = Files.createTempDirectory("tmp").toFile();
-//    try {
-//      ZipUtil.unzip(Base64.decode(base64), tmpDirectory);
-//      File[] files = tmpDirectory.listFiles();
-//      Assertions.assertEquals(1,files.length);
-//
-//      files = files[0].listFiles();
-//      Assertions.assertEquals(2,files.length);
-//      Assertions.assertEquals("PAIN002_2021-11-02-11-12-24_1",files[0].getName());
-//      Assertions.assertEquals("FILE_NAME2",files[1].getName());
-//    }finally {
-//      FileUtils.deleteDirectory(tmpDirectory);
-//    }
-//  }
+  @Test
+  void testGetBase64ZipWithEmptyFilesNames() throws IOException {
+    StringFile file1 = new StringFile("FILE_CONTENT1", FileFormat.PAIN002.name(), "");
+    StringFile file2 = new StringFile("FILE_CONTENT2", FileFormat.PAIN002.name(), "");
+    String base64 = ZipUtil.getBase64Zip(Arrays.asList(file1, file2));
+    Assertions.assertNotNull(base64);
 
-  private File getParentTempDirectory(){
+    final File tmpDirectory = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(Base64.decode(base64), tmpDirectory);
+      File[] files = requireNonNull(tmpDirectory.listFiles());
+      Assertions.assertEquals(1, files.length);
+
+      files = requireNonNull(files[0].listFiles());
+      Assertions.assertEquals(2, files.length);
+
+      Date date = new Date();
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+
+      Assertions.assertEquals(String.format("PAIN002_%s_1", dateFormat.format(date)), files[0].getName());
+      Assertions.assertEquals(String.format("PAIN002_%s_2", dateFormat.format(date)), files[1].getName());
+    } finally {
+      FileUtils.deleteDirectory(tmpDirectory);
+    }
+  }
+
+  private File getParentTempDirectory() {
     try {
       final File tmpDirectory = Files.createTempDirectory("tmp").toFile();
-      try{
+      try {
         return tmpDirectory.getParentFile();
-      }finally {
+      } finally {
         FileUtils.deleteDirectory(tmpDirectory);
       }
     } catch (IOException e) {
@@ -93,10 +100,9 @@ class ZipUtilTest {
   }
 
 
-
   @Test
   void testUnzipFiles() throws URISyntaxException, IOException {
-    URL resource = getClass().getResource("ZippedBase64EDoc.txt");
+    URL resource = requireNonNull(getClass().getResource("ZippedBase64EDoc.txt"));
     byte[] b = IOUtils.toByteArray(Files.newInputStream(Paths.get(resource.toURI())));
 
     final File tmpDirectory = Files.createTempDirectory("tmp").toFile();
@@ -116,14 +122,14 @@ class ZipUtilTest {
   }
 
   private void validateFileSize(File[] files) throws IOException {
-    for(File f : files){
+    for (File f : files) {
       Assertions.assertTrue(Files.size(f.toPath()) > 1000);
     }
   }
 
   @Test
   void testZipFiles() throws URISyntaxException, IOException {
-    URL resource = getClass().getResource("blank.pdf");
+    URL resource = requireNonNull(getClass().getResource("blank.pdf"));
     byte[] b = IOUtils.toByteArray(Files.newInputStream(Paths.get(resource.toURI())));
 
     File tmpDirectory = Files.createTempDirectory("tmp").toFile();
@@ -131,12 +137,12 @@ class ZipUtilTest {
       File folder1 = new File(tmpDirectory, "account 1");
       assertTrue(folder1.mkdir());
 
-      FileUtils.writeByteArrayToFile(new File(folder1, "edoc_1.pdf"),b);
+      FileUtils.writeByteArrayToFile(new File(folder1, "edoc_1.pdf"), b);
 
       File folder2 = new File(tmpDirectory, "account 2");
       assertTrue(folder2.mkdir());
-      FileUtils.writeByteArrayToFile(new File(folder2, "edoc_2.pdf"),b);
-      FileUtils.writeByteArrayToFile(new File(folder2, "edoc_3.pdf"),b);
+      FileUtils.writeByteArrayToFile(new File(folder2, "edoc_2.pdf"), b);
+      FileUtils.writeByteArrayToFile(new File(folder2, "edoc_3.pdf"), b);
 
       byte[] zippedFolder = ZipUtil.zip(tmpDirectory.toPath());
 
@@ -160,12 +166,12 @@ class ZipUtilTest {
 
   @Test
   void testZipSingleFile() throws URISyntaxException, IOException {
-    URL resource = getClass().getResource("blank.pdf");
+    URL resource = requireNonNull(getClass().getResource("blank.pdf"));
     byte[] b = IOUtils.toByteArray(Files.newInputStream(Paths.get(resource.toURI())));
 
     File tmpDirectory = Files.createTempDirectory("tmp").toFile();
     try {
-      FileUtils.writeByteArrayToFile(new File(tmpDirectory, "camt053.txt"),b);
+      FileUtils.writeByteArrayToFile(new File(tmpDirectory, "camt053.txt"), b);
       byte[] zippedFolder = ZipUtil.zip(tmpDirectory.toPath());
 
       //validate zipped file
@@ -183,24 +189,143 @@ class ZipUtilTest {
   }
 
   @Test
-  void testZipContent() throws IOException {
+  void testZipContent() {
     byte[] content = "CONTENT".getBytes(StandardCharsets.UTF_8);
     byte[] result = ZipUtil.zip(content);
     assertNotNull(result);
     byte[] validate = ZipUtil.unzip(result);
-    Assertions.assertArrayEquals(content,validate);
+    Assertions.assertArrayEquals(content, validate);
   }
 
-//  @Test
-//  void testGenerateFile() throws IOException {
-//    String fileName = "PAIN001.xml";
-//    File file = new File(fileName);
-//    file.createNewFile();
-//    final File folder = Files.createTempDirectory("folder").toFile();
-//    File file = File.createTempFile(FilenameUtils.getBaseName(fileName), FilenameUtils.getExtension(fileName));
-//  }
+  @Test
+  void testZipWithFileName() throws IOException {
+    byte[] content = "CONTENT".getBytes(StandardCharsets.UTF_8);
+    byte[] result = ZipUtil.zip(content, "MyFile.txt");
+    assertNotNull(result);
+
+    final File folder = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(result, folder);
+      File[] files = folder.listFiles();
+      assertEquals(1, requireNonNull(files).length);
+      assertEquals("MyFile.txt", files[0].getName());
+    } finally {
+      FileUtils.deleteDirectory(folder);
+    }
+  }
+
+  @Test
+  void testZipWithSubFolderAndSingleFile() throws IOException {
+    byte[] content = "CONTENT".getBytes(StandardCharsets.UTF_8);
+    byte[] result = ZipUtil.zip(content, "subfolder1234/MyFile.txt");
+    assertNotNull(result);
+
+    final File folder = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(result, folder);
+      File[] files = folder.listFiles();
+      assertEquals(1, requireNonNull(files).length);
+      assertEquals("subfolder1234", files[0].getName());
+
+      files = files[0].listFiles();
+      assertEquals(1, requireNonNull(files).length);
+      assertEquals("MyFile.txt", files[0].getName());
+    } finally {
+      FileUtils.deleteDirectory(folder);
+    }
+  }
 
 
+  @Test
+  void testZipWithSubFolderAndFiles() throws IOException {
+    byte[] result = ZipUtil.zip(Arrays.asList(builder()
+                                                  .content("CONTENT_1".getBytes(StandardCharsets.UTF_8))
+                                                  .fileName("subfolder1234/MyFile1.txt")
+                                                  .build(),
+                                              builder()
+                                                  .content("CONTENT_12".getBytes(StandardCharsets.UTF_8))
+                                                  .fileName("subfolder1234/MyFile2.txt")
+                                                  .build()));
+    assertNotNull(result);
+
+    final File folder = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(result, folder);
+      File[] files = folder.listFiles();
+      assertEquals(1, requireNonNull(files).length);
+      assertEquals("subfolder1234", files[0].getName());
+
+      files = files[0].listFiles();
+      assertEquals(2, requireNonNull(files).length);
+      assertEquals("MyFile1.txt", files[0].getName());
+      assertEquals("MyFile2.txt", files[1].getName());
+    } finally {
+      FileUtils.deleteDirectory(folder);
+    }
+  }
+
+  @Test
+  void testZipWithoutFileName() throws IOException {
+    byte[] result = ZipUtil.zip(Arrays.asList(builder()
+                                                  .content("CONTENT_1".getBytes(StandardCharsets.UTF_8))
+                                                  .build(),
+                                              builder()
+                                                  .content("CONTENT_12".getBytes(StandardCharsets.UTF_8))
+                                                  .build()));
+    assertNotNull(result);
+
+    final File folder = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(result, folder);
+      File[] files = folder.listFiles();
+      assertEquals(2, requireNonNull(files).length);
+
+      assertTrue(files[0].getName().endsWith("_1"));
+      assertTrue(files[1].getName().endsWith("_2"));
+    } finally {
+      FileUtils.deleteDirectory(folder);
+    }
+  }
+
+  @Test
+  void testBase64EncodedZipWithMixedContent() throws IOException {
+    String result = ZipUtil.getBase64EncodedZip(Arrays.asList(builder()
+                                                                  .content(Base64.encode("CONTENT_1".getBytes(StandardCharsets.UTF_8)))
+                                                                  .fileName("MyFile1.txt")
+                                                                  .build(),
+                                                              builder()
+                                                                  .content(ZipUtil.zip("CONTENT_12".getBytes(StandardCharsets.UTF_8)))
+                                                                  .fileName("MyFile2.txt")
+                                                                  .build()));
+    final File folder = Files.createTempDirectory("tmp").toFile();
+    try {
+      ZipUtil.unzip(Base64.decode(result), folder);
+      File[] files = folder.listFiles();
+      assertEquals(2, requireNonNull(files).length);
+
+      assertEquals("MyFile1.txt", files[0].getName());
+      assertEquals("CONTENT_1", FileUtils.readFileToString(files[0], StandardCharsets.UTF_8));
+      assertEquals("MyFile2.txt", files[1].getName());
+      assertEquals("CONTENT_12", FileUtils.readFileToString(files[1], StandardCharsets.UTF_8));
+    } finally {
+      FileUtils.deleteDirectory(folder);
+    }
+  }
+
+  @Test
+  void testIsZipFailed() {
+    Assertions.assertFalse(ZipUtil.isZipFile((String) null));
+    Assertions.assertFalse(ZipUtil.isZipFile((byte[]) null));
+    Assertions.assertFalse(ZipUtil.isZipFile("zip"));
+    Assertions.assertFalse(ZipUtil.isZipFile("zip".getBytes(StandardCharsets.UTF_8)));
+  }
+
+  @Test
+  void testIsZip() throws URISyntaxException, IOException {
+    URL resource = requireNonNull(getClass().getResource("TestFile.zip"));
+    byte[] b = IOUtils.toByteArray(Files.newInputStream(Paths.get(resource.toURI())));
+    Assertions.assertTrue(ZipUtil.isZipFile(b));
+  }
 
 
 }
